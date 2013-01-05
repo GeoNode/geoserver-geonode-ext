14631aa2c3df2f22f9534d14ea8d8b64905abde3
Michael Weisman <mweisman@gmail.com>
2013-01-05 12:58:44 -0800

    Copy rpms to build server

--- build_geonode-geoserver-ext-rpm.sh
+++ build_geonode-geoserver-ext-rpm.sh
@@ -3,5 +3,9 @@
 set -e
 
 export GEONODE_EXT_ROOT=$PWD
+DL_ROOT=/var/www/geoserver
+GIT_REV=$(git log -1 --pretty=format:%h)
 
 rpmbuild --define "_topdir ${PWD}/rpm" -bb rpm/SPECS/geoserver.spec
+
+scp -P 7777 -i ../jenkins_key.pem ../*.rpm jenkins@build.geonode.org:$DL_ROOT/$GIT_REV
