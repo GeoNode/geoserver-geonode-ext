cfd0bd28aeccc5002e2c31ccd0388fa2a73c92d3
Ariel Nunez <ingenieroariel@gmail.com>
2013-10-27 13:50:26 -0500

    Moved GeoNode to port 80 instead of 8000 by default in order to create working debian packages

--- src/main/java/org/geonode/security/GeoNodeSecurityProvider.java
+++ src/main/java/org/geonode/security/GeoNodeSecurityProvider.java
@@ -105,7 +105,7 @@ public class GeoNodeSecurityProvider extends GeoServerSecurityProvider implement
         GeoNodeAuthProviderConfig providerConfig = new GeoNodeAuthProviderConfig();
         providerConfig.setName("geonodeAuthProvider");
         providerConfig.setClassName(GeoNodeAuthenticationProvider.class.getCanonicalName());
-        providerConfig.setBaseUrl("http://localhost:8000/");
+        providerConfig.setBaseUrl("http://localhost/");
         manager.saveAuthenticationProvider(providerConfig);
 
         GeoNodeAuthFilterConfig filterConfig = new GeoNodeAuthFilterConfig();
