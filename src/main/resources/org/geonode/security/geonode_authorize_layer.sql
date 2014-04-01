1cd625007b391ab4af83b42487238bdb884464bf
Tyler Garner <garnertb@gmail.com>
2014-04-01 10:41:48 -0400

    Update geonode_authorize_layer.sql to work with groups.

--- src/main/resources/org/geonode/security/geonode_authorize_layer.sql
+++ src/main/resources/org/geonode/security/geonode_authorize_layer.sql
@@ -7,10 +7,18 @@ roles varchar[] = '{anonymous,NULL}';
 ct integer;
 user RECORD;
 layer RECORD;
+groups_enabled BOOLEAN;
 BEGIN
 
+-- see if groups are enabled
+SELECT INTO groups_enabled EXISTS(SELECT *
+            FROM information_schema.tables
+            WHERE TABLE_NAME='groups_group');
+
 -- get the layer and user, take quick action if we can
-SELECT INTO layer "base_resourcebase"."id", "base_resourcebase"."owner_id" FROM "base_resourcebase", "layers_layer" WHERE "base_resourcebase"."id" = "layers_layer"."resourcebase_ptr_id" AND "layers_layer"."typename" = type_name;
+SELECT INTO layer "base_resourcebase"."id", "base_resourcebase"."owner_id"
+FROM "base_resourcebase", "layers_layer"
+WHERE "base_resourcebase"."id" = "layers_layer"."resourcebase_ptr_id" AND "layers_layer"."typename" = type_name;
 if (not FOUND) then
 	-- no layer
 	return 'nl';
@@ -21,6 +29,8 @@ if (user_name IS NOT NULL) then
 		-- no user
 		return 'nu';
 	end if;
+
+
 	if ("user".id = "layer".owner_id) then
 		-- layer owner
 		return 'lo-rw';
@@ -47,6 +57,7 @@ SELECT INTO ct "django_content_type"."id"
 	FROM "django_content_type" 
 	WHERE ("django_content_type"."model" = E'layer'  
 	AND "django_content_type"."app_label" = E'layers' );
+
 
 -- generic role, read-write
 PERFORM "security_genericobjectrolemapping"."object_id" 
@@ -91,6 +102,22 @@ if (user_name IS NOT NULL) then
 		);
 	if (FOUND) then return 'ur-rw'; end if;
 
+  -- user role, user has read-write permissions via group membership
+  if (groups_enabled) then
+  PERFORM "security_groupobjectrolemapping"."object_id"
+		FROM "security_groupobjectrolemapping"
+		INNER JOIN "security_objectrole"
+		ON ("security_groupobjectrolemapping"."role_id" = "security_objectrole"."id")
+		INNER JOIN "security_objectrole_permissions"
+		ON ("security_objectrole"."id" = "security_objectrole_permissions"."objectrole_id")
+		WHERE ("security_objectrole_permissions"."permission_id" = change_perm
+		AND "security_groupobjectrolemapping"."object_ct_id" = ct
+		AND "security_groupobjectrolemapping"."group_id" IN (SELECT DISTINCT(group_id) FROM groups_groupmember WHERE user_id="user".id)
+		AND "security_groupobjectrolemapping"."object_id" = "layer".id
+		);
+	if (FOUND) then return 'ur-rw'; end if;
+	end if;
+
 	-- user role, read-only
 	PERFORM "security_userobjectrolemapping"."object_id" 
 		FROM "security_userobjectrolemapping" 
@@ -98,12 +125,28 @@ if (user_name IS NOT NULL) then
 		ON ("security_userobjectrolemapping"."role_id" = "security_objectrole"."id") 
 		INNER JOIN "security_objectrole_permissions" 
 		ON ("security_objectrole"."id" = "security_objectrole_permissions"."objectrole_id") 
-		WHERE ("security_objectrole_permissions"."permission_id" = change_perm 
+		WHERE ("security_objectrole_permissions"."permission_id" = view_perm
 		AND "security_userobjectrolemapping"."object_ct_id" = ct 
 		AND "security_userobjectrolemapping"."user_id" = "user".id 
 		AND "security_userobjectrolemapping"."object_id" = "layer".id
 		);
 	if (FOUND) then return 'ur-ro'; end if;
+
+	-- user role, user has read-only permissions via group membership
+	if (groups_enabled) then
+  PERFORM "security_groupobjectrolemapping"."object_id"
+		FROM "security_groupobjectrolemapping"
+		INNER JOIN "security_objectrole"
+		ON ("security_groupobjectrolemapping"."role_id" = "security_objectrole"."id")
+		INNER JOIN "security_objectrole_permissions"
+		ON ("security_objectrole"."id" = "security_objectrole_permissions"."objectrole_id")
+		WHERE ("security_objectrole_permissions"."permission_id" = view_perm
+		AND "security_groupobjectrolemapping"."object_ct_id" = ct
+		AND "security_groupobjectrolemapping"."group_id" IN (SELECT DISTINCT(group_id) FROM groups_groupmember WHERE user_id="user".id)
+		AND "security_groupobjectrolemapping"."object_id" = "layer".id
+		);
+	if (FOUND) then return 'ur-ro'; end if;
+	end if;
 end if;
 
 -- uh oh, nothing found
