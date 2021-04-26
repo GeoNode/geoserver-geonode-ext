6e5a88d259ab92c74b375137dd2faf30d53db2c5
Alessandro Parma <alessa.parma@gmail.com>
2021-04-26 15:53:58 +0200

    remove unused ext and update to 2.18.3

--- geoserver/extensions/printng/src/main/java/org/geoserver/printng/spi/PrintSpecException.java
+++
@@ -1,8 +0,0 @@
-package org.geoserver.printng.spi;
-
-@SuppressWarnings("serial")
-public class PrintSpecException extends RuntimeException {
-    public PrintSpecException(String message) {
-        super(message);
-    }
-}
