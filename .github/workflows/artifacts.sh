459ab599f4c0b848d7130c1a3b8e053cdd5b33cd
G.Allegri <giohappy@gmail.com>
2025-04-02 17:52:05 +0200

    Update artifacts.sh

--- .github/workflows/artifacts.sh
+++ .github/workflows/artifacts.sh
@@ -23,6 +23,52 @@ wget https://repo1.maven.org/maven2/org/postgis/postgis-jdbc/1.3.3/postgis-jdbc-
 wget https://maven.geo-solutions.it/org/hibernatespatial/hibernate-spatial-postgis/1.1.3.2/hibernate-spatial-postgis-1.1.3.2.jar -O WEB-INF/lib/hibernate-spatial-postgis-1.1.3.2.jar
 rm WEB-INF/lib/hibernate-spatial-h2-geodb-1.1.3.2.jar 
 
+PATCH="    <security-constraint>
+      <web-resource-collection>
+          <web-resource-name>BlockDemoRequests</web-resource-name>
+          <url-pattern>/TestWfsPost/*</url-pattern>
+      </web-resource-collection>
+      <auth-constraint>
+          <role-name>BLOCKED</role-name>
+      </auth-constraint>
+    </security-constraint>
+    <security-constraint>
+      <web-resource-collection>
+          <web-resource-name>BlockGWC</web-resource-name>
+          <url-pattern>/gwc/*</url-pattern>
+      </web-resource-collection>
+      <auth-constraint>
+          <role-name>BLOCKED</role-name>
+      </auth-constraint>
+    </security-constraint>
+    <security-constraint>
+      <web-resource-collection>
+          <web-resource-name>AllowGWC_Demo</web-resource-name>
+          <url-pattern>/gwc/demo/*</url-pattern>
+      </web-resource-collection>
+    </security-constraint>
+    <security-constraint>
+      <web-resource-collection>
+          <web-resource-name>AllowGWC_Services</web-resource-name>
+          <url-pattern>/gwc/service/*</url-pattern>
+      </web-resource-collection>
+    </security-constraint>
+    <security-constraint>
+      <web-resource-collection>
+          <web-resource-name>AllowGWC_Rest</web-resource-name>
+          <url-pattern>/gwc/rest/*</url-pattern>
+      </web-resource-collection>
+    </security-constraint>"
+
+awk -v block="$PATCH" '
+/<\/web-app>/ {
+  print block
+}
+{ print }
+' WEB-INF/web.xml > WEB-INF/web_patched.xml
+
+mv WEB-INF/web_patched.xml WEB-INF/web.xml
+
 cp -r ../geoserver-geonode-ext/data ./
 zip -r ../artifacts/geoserver.war ./
 popd
