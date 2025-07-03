98a16e4ae877c381dcf4585f1e51b800b878dd7e
G.Allegri <giohappy@gmail.com>
2025-07-03 19:04:18 +0200

    No need to postgis and hibernate spatial jars since 2.27.1

--- .github/workflows/artifacts.sh
+++ .github/workflows/artifacts.sh
@@ -18,11 +18,6 @@ unzip ../geoserver_vanilla.war
 ## copy the new ones
 cp -v ../geoserver-geonode-ext/libs/* WEB-INF/lib/
 
-## replace H2 hibernate with PostGIS for the Geofence rules DB
-wget https://repo1.maven.org/maven2/org/postgis/postgis-jdbc/1.3.3/postgis-jdbc-1.3.3.jar -O WEB-INF/lib/postgis-jdbc-1.3.3.jar
-wget https://maven.geo-solutions.it/org/hibernatespatial/hibernate-spatial-postgis/1.1.3.2/hibernate-spatial-postgis-1.1.3.2.jar -O WEB-INF/lib/hibernate-spatial-postgis-1.1.3.2.jar
-rm WEB-INF/lib/hibernate-spatial-h2-geodb-1.1.3.2.jar 
-
 cp -r ../geoserver-geonode-ext/data ./
 zip -r ../artifacts/geoserver.war ./
 popd
