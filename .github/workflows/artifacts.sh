0ad7006791d09ee9a1aa72e602768cf1fac16248
G.Allegri <giohappy@gmail.com>
2025-11-14 17:05:55 +0100

    Include Geofence 3.8.2

--- .github/workflows/artifacts.sh
+++ .github/workflows/artifacts.sh
@@ -15,6 +15,9 @@ mkdir _tmp
 pushd _tmp
 unzip ../geoserver_vanilla.war
 
+### We bring in 3.8.2 that wasn't included in GS 2.27.3
+rm WEB-INF/lib/geofence-services-impl-3.8.1.jar
+
 ## copy the new ones
 cp -v ../geoserver-geonode-ext/libs/* WEB-INF/lib/
 
